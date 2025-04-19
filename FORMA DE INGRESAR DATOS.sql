f2e66816ea7b9ce97a7732b99e6aa295467d210f
Jhaseft <jhasesaat@gmail.com>
2025-04-18 22:29:00 -0400

    Update FORMA DE INGRESAR DATOS.sql

--- FORMA DE INGRESAR DATOS.sql
+++ FORMA DE INGRESAR DATOS.sql
@@ -1,15 +1,15 @@
 --FORMA DE INGRESAR DATOS A LA BD , SI NO ENTIENDES ALGUN ATRIBUTO APOYATE CON LOS MODELOS DE POWER DESIGNER
 --insertar cliente con su o sus autos 
 --Datos
-INSERT INTO Cliente VALUES ('76452345', 'María Valencia', '78781234', 'Av. Las Flores 123', 'maria.Valencia@email.com');
+INSERT INTO Cliente VALUES ('76452345', 'MarÃ­a Valencia', '78781234', 'Av. Las Flores 123', 'maria.Valencia@email.com');
 --Autos
 INSERT INTO Auto VALUES ('8899KGD', '76452345', 'Toyota', 'Rav 4', 'Blanco');
 INSERT INTO Auto VALUES ('4120AZQ', '76452345', 'Suzuki', 'Swift', 'Rojo');
 INSERT INTO Auto VALUES ('1247ASQ', '76452345', 'Toyota', 'Rav 4', 'Gris');
 --Fallas
 INSERT INTO Fallas VALUES ('F0001', '8899KGD', 'Frenos', 'Chirrido al frenar', 'Media');
-INSERT INTO Fallas VALUES ('F0002', '4120AZQ', 'Motor', 'Pérdida de potencia', 'Alta');
-INSERT INTO Fallas VALUES ('F0003', 'STAVVC', 'Suspensión', 'Ruido en baches', 'Baja');
+INSERT INTO Fallas VALUES ('F0002', '4120AZQ', 'Motor', 'PÃ©rdida de potencia', 'Alta');
+INSERT INTO Fallas VALUES ('F0003', 'STAVVC', 'SuspensiÃ³n', 'Ruido en baches', 'Baja');
 --Diagnosticos 
 INSERT INTO Diagnostico VALUES ('D00001', 'F0001', 'Pastillas de freno desgastadas al 80%');
 INSERT INTO Diagnostico VALUES ('D00002', 'F0002', 'Filtro de combustible obstruido');
@@ -37,9 +37,9 @@ INSERT INTO Repuestos VALUES ('Repu11', 'Inve122', 'Pastillas de freno', 'T', 1,
 INSERT INTO Repuestos VALUES ('Repu12', 'Inve222', 'Filtro de combustible', 'T', 1, 45.00);
 INSERT INTO Repuestos VALUES ('Repu13', '0', 'Amortiguador trasero', 'C', 1, 480.00);
 --Reparaciones realizadas
-INSERT INTO Reparacion VALUES ('Repa11', 'OD001', 'Repu11', 'Pastillas de freno desgastadas al 80%');
-INSERT INTO Reparacion VALUES ('Repa12', 'OD002', 'Repu12', 'Filtro de combustible obstruido');
-INSERT INTO Reparacion VALUES ('Repa13', 'OD003', 'Repu13', 'Amortiguadores traseros gastados');
+INSERT INTO Reparacion VALUES ('Repa11', 'OD001', 'Repu11', 'Pastillas cambiadas');
+INSERT INTO Reparacion VALUES ('Repa12', 'OD002', 'Repu12', 'Filtro restaurado , obstruccion por metal');
+INSERT INTO Reparacion VALUES ('Repa13', 'OD003', 'Repu13', 'amortiguadores cambiados');
 --Quien hizo las reaparaciones, un empleado puede hacer muchas reparaciones y una reparacion puede ser hecha por muchos empleados
 INSERT INTO Efectua VALUES ('628471', 'Repa11');
 INSERT INTO Efectua VALUES ('753429', 'Repa11');
